705c0789fcbdd125f362299486e4cb2d26fff629
gioStam77 <62613146+gioStam77@users.noreply.github.com>
2023-11-15 13:52:11 +0200

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
-RANDOM_NUMBER=$(($RANDOM %1000))
+RANDOM_NUMBER=$(( $RANDOM % 1000 + 1))
+
 
 echo -e "\nEnter your username:"
 read USERNAME
 
-ID=$($PSQL "SELECT user_id FROM users WHERE user_name='$USERNAME'");
-GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$ID");
-BEST_GAME=$($PSQL "SELECT MIN(num_guesses)FROM games WHERE user_id=$ID");
-if [[ -z $ID ]]
+DB_USERNAME=$($PSQL "SELECT user_name FROM users WHERE user_name='$USERNAME'");
+
+if [[ -z $DB_USERNAME ]]
 then
   INSERT_NEW_PLAYER=$($PSQL "INSERT INTO users(user_name)VALUES('$USERNAME')");
   echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
 else
-  echo -e "\nWelcome back,$USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  ID=$($PSQL "SELECT user_id FROM users WHERE user_name='$USERNAME'");
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(DISTINCT game_id) FROM games WHERE user_id=$ID");
+  BEST_GAME=$($PSQL "SELECT MIN(num_guesses)FROM games WHERE user_id=$ID");
+  #echo -e "\nWelcome back,$USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+ # echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  echo -e  "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 echo -e "\nGuess the secret number between 1 and 1000:"
 read INPUT_NUMBER
@@ -39,6 +44,10 @@ else
       read INPUT_NUMBER
       fi 
       done  
-     INSERTED_GAME=$($PSQL "INSERT INTO games(user_id,num_guesses)VALUES($ID,$NUM_OF_GUESSES)"); 
-     echo -e You guessed it in $NUM_OF_GUESSES tries. The secret number was $RANDOM_NUMBER. Nice job!
 fi
+   
+     ID=$($PSQL "SELECT user_id FROM users WHERE user_name='$USERNAME'");
+     INSERTED_GAME=$($PSQL "INSERT INTO games(user_id,num_guesses)VALUES($ID, $NUM_OF_GUESSES)"); 
+     echo -e "\nYou guessed it in $NUM_OF_GUESSES tries. The secret number was $RANDOM_NUMBER. Nice job!\n"
+
+ 
